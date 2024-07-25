eea3ea651f87ff7cda75eb67f6b5514c50b9fcfc
saifhasan14 <binal0818@gmail.com>
2024-07-25 18:53:06 +0530

    fix bug

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -5,7 +5,7 @@ export default defineConfig({
   plugins: [react()],
   server: {
     proxy: {
-      '/api': 'http://localhost:8000/api/v1'
+      '/api': 'https://social-tube-backend.vercel.app/api/v1'
     }
   }
 })
